be9842e99e0f86ada9bc524dd29b0fba598305a1
Eric Kanski <erickanski0402@gmail.com>
2019-02-10 13:54:52 -0600

    Added documentation for initial design of network

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,3 +1,10 @@
+# Initial design for neural network:
+#   Inputs (I): 784 nodes (for each pixel value in dataset)
+#   Hidden Layer 1 (H1): 16 nodes, each receiving input from all 784 input nodes
+#   Hidden Layer 2 (H2): 16 nodes, each receiving input from all 16 H1 nodes
+#   Outputs (0): 10 nodes (for each possible digit label)
+#
+
 import MNIST_Dataset as mnist
 
 # Loads the full 42,000 rows of test data
